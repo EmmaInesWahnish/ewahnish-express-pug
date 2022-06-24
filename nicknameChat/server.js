7b51289e86bc2f7f68033103273af9a356bf928a
Emma Wahnish <ewahnish@gmail.com>
2022-06-24 20:17:43 -0300

    Last Change

--- nicknameChat/server.js
+++ nicknameChat/server.js
@@ -47,7 +47,7 @@ io.on('connection', (socket) => {
 
     socket.on('chat message', (msg) => {
         //console.log('message: ' + msg);
-        socket.emit('chat message', msg);
+        io.emit('chat message', msg);
         addToMessageList(msg)
     })
 
