2c19ba08689b8ce8cf01325ec0415e1c7ec5103b
Emma Wahnish <ewahnish@gmail.com>
2022-08-19 20:34:34 -0300

    avatars

--- mockAndNormalization/src/js/generateMessage.js
+++ mockAndNormalization/src/js/generateMessage.js
@@ -1,17 +1,80 @@
-const faker = require('faker')
+const faker = require('faker');
 
 faker.locale = "es";
 
+const avatars = [
+    '/images/avatar01F.png',
+    '/images/avatar02F.png',
+    '/images/avatar03F.png',
+    '/images/avatar04F.png',
+    '/images/avatar05F.png',
+    '/images/avatar06F.png',
+    '/images/avatar01M.png',
+    '/images/avatar02M.png',
+    '/images/avatar03M.png',
+    '/images/avatar04M.png',
+    '/images/avatar05M.png',
+    '/images/avatar06M.png'    
+];
+
+const emails = [
+    'af@gmail.com',
+    'bf@gmail.com',
+    'cf@gmail.com',
+    'df@gmail.com',
+    'ef@gmail.com',
+    'ff@gmail.com',
+    'am@gmail.com',
+    'bm@gmail.com',
+    'cm@gmail.com',
+    'dm@gmail.com',
+    'em@gmail.com',
+    'fm@gmail.com'
+];
+
+const nombres = [
+    'Lucia',
+    'Maria',
+    'Candelaria',
+    'Sofia',
+    'Guadalupe',
+    'Francisca',
+    'Jose',
+    'Roberto',
+    'Manuel',
+    'Maximiliano',
+    'Jorge',
+    'Pablo'
+];
+
+const alias = [
+    'Lucy',
+    'Mari',
+    'Cande',
+    'Sofi',
+    'Lupe',
+    'Paca',
+    'Joe',
+    'Rober',
+    'Manu',
+    'Maxi',
+    'George',
+    'Paul'
+];
+
+
 const generateMessage = () => {
+    let i = Math.floor(Math.random()*11)
+    console.log("Avatar array number ",i)
     let generated_message = {
         timestamp: faker.datatype.datetime(),
         author:{
-        email: faker.internet.email(),
-        nombre: faker.name.firstName(),
+        email: emails[i],
+        nombre: nombres[i],
         apellido: faker.name.lastName(),
-        edad: faker.datatype.number({min: 20, max: 80}),
-        alias: faker.internet.userName(),
-        avatar: faker.image.image(),
+        edad: faker.datatype.number({min: 20, max: 45}),
+        alias: alias[i],
+        avatar: avatars[i],
         },
         text: faker.lorem.sentence()
     }
--- mockAndNormalization/src/routers/routerTestMessages.js
+++ mockAndNormalization/src/routers/routerTestMessages.js
@@ -7,17 +7,23 @@ const MessagesTest = new MessagesDao();
 
 
 routerTestMessages.get('/', async (req, res) => {
-    let generated_messages = []
-    try {
-        generated_messages = await MessagesTest.populate(req.query.cant);
-        res.render('messages.hbs', { generated_messages })        
+    let generated_messages = [];
+    for (let i = 0; i < 4; i++) {
+        let message_group =[]
+        try {
+            message_group = await MessagesTest.populate(req.query.cant);
+            for (let j= 0; j < message_group.length; j++){
+                generated_messages.push(message_group[j]);
+            }
+        }
+        catch (error) {
+            res.json({
+                message: 'No se ha podido recuperar la lista de items de prueba',
+                error: error
+            })
+        }
     }
-    catch (error) {
-        res.json({
-            message: 'No se ha podido recuperar la lista de items de prueba',
-            error: error
-        })
-    }
+    res.render('messages.hbs', { generated_messages })
 })
 
 module.exports = routerTestMessages
