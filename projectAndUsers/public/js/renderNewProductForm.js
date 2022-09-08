6320c0ae9d297f205cbf25c4002f8e125ba33780
Emma Wahnish <ewahnish@gmail.com>
2022-09-08 10:25:59 -0300

    polishing

--- projectAndUsers/public/js/renderNewProductForm.js
+++ projectAndUsers/public/js/renderNewProductForm.js
@@ -3,38 +3,38 @@ import renderHome from './renderHome.js';
 let product = {};
 const renderNewProductForm = () => {
 
-    document.getElementById('activeCart').innerHTML = "";
-    document.getElementById('cartNumber').innerHTML = "";
-    document.getElementById('productCards').innerHTML = "";
-    document.getElementById('newProduct').innerHTML = "";
-    document.getElementById('oneProduct').innerHTML = "";
-    document.getElementById('myCart').innerText = "";
-    document.getElementById('productsInCart').innerHTML = "";
-    document.getElementById('login').innerHTML = "";
-    document.getElementById('register').innerHTML = "";
-    document.getElementById('logout').innerHTML = "";
-    
-    const homePage = document.getElementById("homePage")
-    
-    let show = function (elem) {
-        elem.style.display = 'block';
-    };
-    let hide = function (elem) {
-        elem.style.display = 'none';
-    };
+  document.getElementById('activeCart').innerHTML = "";
+  document.getElementById('cartNumber').innerHTML = "";
+  document.getElementById('productCards').innerHTML = "";
+  document.getElementById('newProduct').innerHTML = "";
+  document.getElementById('oneProduct').innerHTML = "";
+  document.getElementById('myCart').innerText = "";
+  document.getElementById('productsInCart').innerHTML = "";
+  document.getElementById('login').innerHTML = "";
+  document.getElementById('register').innerHTML = "";
+  document.getElementById('logout').innerHTML = "";
 
-    hide(homePage)
+  const homePage = document.getElementById("homePage")
 
-    fetch('/api/productos/isadmin')
+  let show = function (elem) {
+    elem.style.display = 'block';
+  };
+  let hide = function (elem) {
+    elem.style.display = 'none';
+  };
+
+  hide(homePage)
+
+  fetch('/api/productos/isadmin')
     .then(res => res.json())
     .then(data => {
 
-        if (data.bool) {
+      if (data.bool) {
 
-    const newProduct = document.getElementById('newProduct');
-    const productForm = document.createElement('div');
-    productForm.setAttribute('class', 'jumbotron');
-    productForm.innerHTML = `<h2>Alta de producto</h2>
+        const newProduct = document.getElementById('newProduct');
+        const productForm = document.createElement('div');
+        productForm.setAttribute('class', 'jumbotron');
+        productForm.innerHTML = `<h2>Alta de producto</h2>
     <br>
     <form id="productForm" action="javascript:void(0);" id="addProductForm">
 
@@ -71,54 +71,54 @@ const renderNewProductForm = () => {
       <button type="submit" class="btn btn-success" id="addProductButton">Enviar</button>
     </form>`
 
-    newProduct.appendChild(productForm);
+        newProduct.appendChild(productForm);
 
-    let nomb = document.getElementById("nomb");
-    let desc = document.getElementById("desc");
-    let code = document.getElementById("code");
-    let photo = document.getElementById("photo");
-    let price = document.getElementById("price");
-    let quantity = document.getElementById("quantity");
-  
-    nomb.addEventListener('change', function () {
-      product.nombre = document.getElementById("nomb").value;
-    })
-    desc.addEventListener('change', function () {
-      product.descripcion = document.getElementById("desc").value;
-    })
-    code.addEventListener('change', function () {
-      product.codigo = document.getElementById("code").value;
-    })
-    photo.addEventListener('change', function () {
-      product.foto = document.getElementById("photo").value;
-    })
-    price.addEventListener('change', function () {
-      product.precio = document.getElementById("price").value;
-    })
-    quantity.addEventListener('change', function () {
-      product.stock = document.getElementById("quantity").value;
-    })
-  
-    let formAdd = document.getElementById("addProductButton");
-  
-    formAdd.addEventListener('click', function () {
-      let addedProduct = {
-        nombre: product.nombre,
-        descripcion: product.descripcion,
-        codigo: product.codigo,
-        foto: product.foto,
-        precio: product.precio,
-        stock: product.stock,
+        let nomb = document.getElementById("nomb");
+        let desc = document.getElementById("desc");
+        let code = document.getElementById("code");
+        let photo = document.getElementById("photo");
+        let price = document.getElementById("price");
+        let quantity = document.getElementById("quantity");
+
+        nomb.addEventListener('change', function () {
+          product.nombre = document.getElementById("nomb").value;
+        })
+        desc.addEventListener('change', function () {
+          product.descripcion = document.getElementById("desc").value;
+        })
+        code.addEventListener('change', function () {
+          product.codigo = document.getElementById("code").value;
+        })
+        photo.addEventListener('change', function () {
+          product.foto = document.getElementById("photo").value;
+        })
+        price.addEventListener('change', function () {
+          product.precio = document.getElementById("price").value;
+        })
+        quantity.addEventListener('change', function () {
+          product.stock = document.getElementById("quantity").value;
+        })
+
+        let formAdd = document.getElementById("addProductButton");
+
+        formAdd.addEventListener('click', function () {
+          let addedProduct = {
+            nombre: product.nombre,
+            descripcion: product.descripcion,
+            codigo: product.codigo,
+            foto: product.foto,
+            precio: product.precio,
+            stock: product.stock,
+          }
+          addOneProduct(addedProduct);
+
+        })
+
+      } else {
+        alert("Operacion no autorizada")
+        renderHome()
       }
-      addOneProduct(addedProduct);
-  
     })
-
-  } else {
-    alert("Operacion no autorizada")
-    renderHome()
-  }
-})
 
 
 
