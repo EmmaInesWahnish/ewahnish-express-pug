9b4b459527cdedbc8eb6f873e42740e3dd90cca9
Emma Wahnish <ewahnish@gmail.com>
2022-09-11 00:32:37 -0300

    fork

--- loginPassportDotenvInfo/src/routers/randomsRouter.js
+++ loginPassportDotenvInfo/src/routers/randomsRouter.js
@@ -1,24 +1,16 @@
 const express = require('express');
-const processRandoms = require('../js/processRandoms.js')
+const { fork } = require('child_process');
 
 const randomsRouter = express.Router();
 
 randomsRouter.get('/', async (req, res) => {
-
-    let object = await processRandoms(100);
-
-    try {
-        res.json({
-            message: 'Numeros al azar',
-            randomArray: object
+    const child = fork("childProcess.js");
+    child.send('Start', req.query.cant)
+    child.on('message', randomNoCeros => {
+        res.send({
+            randoms: randomNoCeros,
         });
-    }
-    catch (error) {
-        res.json({
-            message: 'No se ha podido recuperar informacion',
-            error: error
-        })
-    }
+    })
 
 })
 
