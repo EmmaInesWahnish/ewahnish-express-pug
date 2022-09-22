b4b16f4da94a89b535d8cbedabdd1465b26ff790
Emma Wahnish <ewahnish@gmail.com>
2022-09-22 16:17:30 -0300

    configuring

--- nginx-1.23.1/Server/src/server.js
+++ nginx-1.23.1/Server/src/server.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(__dirname + '/public'));
+//app.use(express.static(__dirname + '/public'));
 
 app.use('/api/randoms',randomsRouter);
 app.use('/api/anotherRandoms',anotherRandomsRouter);
