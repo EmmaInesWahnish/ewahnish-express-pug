e3c2d685162262ec4c35cbaabd9a8b36538ec950
Emma Wahnish <ewahnish@gmail.com>
2022-10-03 10:27:35 -0300

    initial

---
+++ ProjectUsersLogger/src/daos/ChatDaoMongoDb.js
@@ -0,0 +1,35 @@
+
+import mongoose from 'mongoose';
+import envs from '../../dotenvConfig.js'
+import MessageModel from '../models/messages.js';
+import MongoDbContainer from '../api/MongoDbContainer.js';
+
+const URL = envs.URL.toString();
+const TheModel = MessageModel;
+
+const connectToDb = async () => {
+    try {
+        let rta = await mongoose.connect(URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        console.log("Estado de la conexion ", mongoose.connection.readyState);
+    } catch (error) {
+        console.error("DB Error: ", error);
+    }
+}
+
+class ChatDaoMongoDb extends MongoDbContainer {
+
+    constructor() {
+        super()
+        this.connectToDb = connectToDb;
+        this.TheModel = TheModel;
+    }
+
+    async disconnect() {
+
+    }
+}
+
+export default ChatDaoMongoDb;
